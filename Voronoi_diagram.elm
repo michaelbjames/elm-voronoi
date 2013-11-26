3a0473385639ba11b9fefba63b021bc16b63594b
Michael James <michael@michaelbjames.com>
2013-11-26 17:18:31 -0500

    I can move much of the window fitting to the impure code

--- Voronoi_diagram.elm
+++ Voronoi_diagram.elm
@@ -14,22 +14,24 @@ region xdim ydim sites current =
              <| map (\s -> dist point current <= dist point s) compsites) space
              
 drawCircles color (x,y) =
-  circle 5.0 |> filled color
-             |> move (11*x - 200,-11*y + 250)
+  circle 0.4 |> filled color
+             |> move (x,y)
 
 colorpick x = rgb x x x
 
 mkcircles (x,y) cindex origins =
   let angle = degrees (50 * toFloat cindex)
       color = (hsv angle 1 1)
-  in group <| map (drawCircles color) origins
+  in scale 11.0
+      <| group
+      <| map (drawCircles color) origins
  
 scene (w',h') position = 
   let xdim = 50
       ydim = 50
       pset = position :: [(40,10),(10,40),(0,0),(0,30),(10,10),(20,30),(10,20),(30,50)]
   in collage w' h'
-      <| map (\(x,c) -> mkcircles x c (region xdim ydim pset x))
+      <| map (\(x,c) -> move (-1*toFloat w' /3.0,-1*toFloat h'/3.0) <| mkcircles x c (region xdim ydim pset x))
       <| (zip pset [1..20])
 
 main = lift2 scene Window.dimensions Mouse.position
