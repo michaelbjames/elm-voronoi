6c4da7544bbb518702c7d336b3e130bd5ef2889e
Michael James <michael@michaelbjames.com>
2013-11-25 23:56:37 -0500

    playing around with settings

--- Voronoi_diagram.elm
+++ Voronoi_diagram.elm
@@ -12,21 +12,21 @@ region dimensions sites current =
              <| map (\s -> dist point current <= dist point s) compsites) space
              
 drawCircles color (x,y) =
-  ngon 6 4.0 |> filled color
-             |> move (10*x,10*y)
+  circle 4.0 |> filled color
+             |> move (2*x,2*y)
 
 colorpick x = rgb x x x
 
 mkcircles (x,y) cindex origins =
-  let angle = degrees (60 * toFloat cindex)
+  let angle = degrees (20 * toFloat cindex)
       color = (hsv angle 0.7 1)
   in group <| map (drawCircles color) origins
  
 scene (w,h) = 
-  let maxdim = 10
-      pset = [(9,1),(1,9), (9,9)]
-  in container w h middle
-      <| collage 300 300
+  let maxdim = 150
+      pset = [(90,100),(100,90),(0,0),(0,30),(100,60)]
+  in container w h midTop
+      <| collage 900 900
       (map (\(x,c) -> mkcircles x c (region maxdim pset x))
         <| (zip pset [1..20]))
 
