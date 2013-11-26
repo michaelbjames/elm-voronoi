38edf5a686fad4c6df1aa635c6e089a4f9673131
Michael James <michael@michaelbjames.com>
2013-11-26 12:50:02 -0500

    whitespacing looks cool

--- Voronoi_diagram.elm
+++ Voronoi_diagram.elm
@@ -13,20 +13,20 @@ region xdim ydim sites current =
              <| map (\s -> dist point current <= dist point s) compsites) space
              
 drawCircles color (x,y) =
-  circle 4.0 |> filled color
-             |> move (x - 200,y - 200)
+  circle 1.0 |> filled color
+             |> move (3*x - 200,3*y - 250)
 
 colorpick x = rgb x x x
 
 mkcircles (x,y) cindex origins =
-  let angle = degrees (150 * toFloat cindex)
-      color = (hsv angle 0.7 1)
+  let angle = degrees (50 * toFloat cindex)
+      color = (hsv angle 1 1)
   in group <| map (drawCircles color) origins
  
 scene (w',h') = 
   let xdim = 200
       ydim = 200
-      pset = [(90,100),(100,90),(0,0),(0,30),(100,60)]
+      pset = [(90,100),(100,90),(0,0),(0,30),(100,60),(170,30),(100,170),(30,150)]
   in collage w' h'
       <| map (\(x,c) -> mkcircles x c (region xdim ydim pset x))
       <| (zip pset [1..20])
