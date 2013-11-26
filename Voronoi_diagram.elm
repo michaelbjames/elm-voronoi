194e41d707833f5bc01922383df5c376a88d7968
Michael James <michael@michaelbjames.com>
2013-11-26 00:15:53 -0500

    making it react!

--- Voronoi_diagram.elm
+++ Voronoi_diagram.elm
@@ -2,12 +2,13 @@ import Random
 import Signal
 import Window
 
+--Manhattan Distance function
 dist (x1,y1) (x2,y2) = abs (x1-x2) + abs (y1-y2)
 
-region dimensions sites current =
+region xdim ydim sites current =
   let compsites = filter (\r -> r /= current) sites
-      space = foldr (\x base -> (map (\y -> (x,y)) [0..dimensions]) ++ base) []
-        [0..dimensions]
+      space = foldr (\x base -> (map (\y -> (x,y)) [0..ydim]) ++ base) []
+        [0..xdim]
   in filter (\point -> foldl (&&) True 
              <| map (\s -> dist point current <= dist point s) compsites) space
              
@@ -23,11 +24,12 @@ mkcircles (x,y) cindex origins =
   in group <| map (drawCircles color) origins
  
 scene (w,h) = 
-  let maxdim = 150
+  let xdim = w/4
+      ydim = h/4
       pset = [(90,100),(100,90),(0,0),(0,30),(100,60)]
-  in container w h midTop
-      <| collage 900 900
-      (map (\(x,c) -> mkcircles x c (region maxdim pset x))
+  in container w h middle
+      <| collage w h
+      (map (\(x,c) -> mkcircles x c (region xdim ydim pset x))
         <| (zip pset [1..20]))
 
 main = lift scene Window.dimensions
